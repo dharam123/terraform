3a7d62a0db64b69dc3efa7f648a92fe006a377e1
dharam123 <d2yadava@gmail.com>
2023-01-03 10:38:00 +0530

    Update tfm.tf

--- tfm.tf
+++ tfm.tf
@@ -4,14 +4,14 @@ provider "aws" {
 
 
 resource "aws_instance" "web" {
-   ami = "ami-0ca285d4c2cda3300"
+   ami = "ami-0b5eea76982371e91"
    instance_type = "t2.micro"
    key_name = "terraform-key"
 
    connection {
    type     = "ssh"
    user     = "ec2-user"
-   private_key = file("./terraform-key.pem")
+   private_key = file("./trfm.pem")
    host     = self.public_ip
     }
 
