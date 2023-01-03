025e45ded324aa7ee67136637348eefd2ad90c6c
dharam123 <d2yadava@gmail.com>
2023-01-03 16:15:30 +0530

    Update tfm.tf

--- tfm.tf
+++ tfm.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "web" {
    inline = [
      "sudo amazon-linux-extras install -y nginx1",
      "sudo systemctl start nginx",
-     "echo '<h1><center> Welcome to TGS </center</h1>' > index.html"
+     "echo '<h1><center> Welcome to TGS </center</h1>' > index.html",
 "sudo mv index.html  /usr/share/nginx/html/"
 
  
