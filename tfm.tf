0a789669dbc52c664590d45a878e679dc27ee830
dharam123 <d2yadava@gmail.com>
2023-01-04 09:27:13 +0530

    Update tfm.tf

--- tfm.tf
+++ tfm.tf
@@ -20,7 +20,11 @@ resource "aws_instance" "web" {
      "sudo amazon-linux-extras install -y nginx1",
      "sudo systemctl start nginx",
      "sudo yum install -y git",
-     "git clone https://github.com/dharam123/hello-world.git"
+     "git clone https://github.com/dharam123/hello-world.git",
+     "cd /usr/share/nginx/html",
+     "sudo cp index.jsp index.html",
+     "sudo systemctl restart nginx"
+   
     
    ]
  }
